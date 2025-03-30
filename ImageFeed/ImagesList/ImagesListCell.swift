24b8b05b7aab882704b7a71d999827e662f9492b
Almas <almas.zhartybayev@gmail.com>
2025-03-30 16:41:36 +0500

    Сверстан экран Launch screen, добавлен и сверстан экран ленты изображений

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Foundation
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+ 
+    @IBOutlet weak var likeButton: UIButton!
+    
+}
+
